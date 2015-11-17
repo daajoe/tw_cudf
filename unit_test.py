f5bc100747e00363caecbb962b6598c94efc50c0
daajoe <daajoe@users.noreply.github.com>
2015-11-17 17:35:05 +0100

    Delete unit_test.py

--- unit_test.py
+++
@@ -1,8 +0,0 @@
-
-#move to unit
-#print operator('asdf=2')
-#print operator('glass < 2')
-#print operator('glass = 2')
-#print operator('asdf<=3')
-#print operator('asdf<3')
-#print operator('asdf')
